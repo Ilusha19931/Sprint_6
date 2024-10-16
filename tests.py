3902175b987c7179efeeace0ea8d62216a9b6e89
Ilya Mzokov <Tayes134@yandex.ru>
2024-10-16 14:30:40 +0300

    Работа по юнитам

--- tests.py
+++ tests.py
@@ -1,24 +1,71 @@
+import pytest
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
     def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
-
-        # добавляем две книги
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(collector.get_books_rating()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    @pytest.mark.parametrize("book_name", [
+        "",
+        "12345678901234567890123456789012345678901234"
+    ])
+    def test_add_new_book_invalid_book_name(self, book_name):
+        collector = BooksCollector()
+        collector.add_new_book(book_name)
+        assert book_name not in collector.get_books_genre()
+
+    def test_set_book_genre_valid_name_add_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book("Светлое будущее")
+        collector.set_book_genre("Светлое будущее", "Фантастика")
+        assert collector.get_book_genre("Светлое будущее") == "Фантастика"
+
+    def test_set_book_genre_invalid_unnamed(self):
+        collector = BooksCollector()
+        collector.set_book_genre("Некрономикон", "Фантастика")
+        assert collector.get_book_genre("Некрономикон") is None
+
+    def test_get_books_with_specific_genre_valid_add_in_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book("Ужасная книга")
+        collector.set_book_genre("Ужасная книга", "Ужасы")
+        collector.add_new_book("Приключенческая книга")
+        collector.set_book_genre("Приключенческая книга", "Комедии")
+
+        assert collector.get_books_with_specific_genre("Ужасы") == ["Книга Ужасов"]
+        assert collector.get_books_with_specific_genre("Комедии") == ["Приключенческая Книга"]
+        assert collector.get_books_with_specific_genre("Фантастика") == []
+
+    def test_get_books_for_children(self):
+        collector = BooksCollector()
+        collector.add_new_book("Книга Ужасов")
+        collector.set_book_genre("Книга Ужасов", "Ужасы")
+        collector.add_new_book("Детская Книга")
+        collector.set_book_genre("Детская Книга", "Фантастика")
+
+        assert collector.get_books_for_children() == ["Детская Книга"]
+
+    def test_add_book_in_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book("Тестовая Книга")
+        collector.add_book_in_favorites("Тестовая Книга")
+        assert "Тестовая Книга" in collector.get_list_of_favorites_books()
+
+    def test_delete_book_from_favorites_valid_deleted_name(self):
+        collector = BooksCollector()
+        collector.add_new_book("Тестовая Книга")
+        collector.add_book_in_favorites("Тестовая Книга")
+        collector.delete_book_from_favorites("Тестовая Книга")
+        assert "Тестовая Книга" not in collector.get_list_of_favorites_books()
+
+    def test_get_list_of_favorites_books_included_two_books(self):
+        collector = BooksCollector()
+        collector.add_new_book("Тестовая Книга")
+        collector.add_book_in_favorites("Тестовая Книга")
+        collector.add_new_book("Тестовая Книга2")
+        collector.add_book_in_favorites("Тестовая Книга2")
+        assert collector.get_list_of_favorites_books() == ["Тестовая Книга"]
